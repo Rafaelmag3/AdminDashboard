cd1f92493d12d6bafc051bf1db582069b727135d
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-04-04 12:22:07 -0600

    feat: add MENU_CONSTANTS for device types and implement responsive backdrop in menu component

---
+++ src/app/constants/menu.constants.ts
@@ -0,0 +1,5 @@
+export const MENU_CONSTANTS = {
+    MOBILE: 'mobile',
+    TABLET: 'tablet',
+    DESKTOP: 'desktop'
+} as const;
--- src/app/shared/menu/menu.ts
+++ src/app/shared/menu/menu.ts
@@ -1,14 +1,16 @@
-import { Component, computed, inject, OnInit, signal, viewChild } from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal, viewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MenuItem } from '@shared/models/menu.interface';
-import { ICONS } from 'src/app/constants/icons.contants';
+import { ICONS } from '@constants/icons.contants';
 import { MenuService } from '@core/services/menu-service';
 import { NavigationService } from '@core/services/navigation-service';
 import { environment } from '@env/environment';
 import { Header } from '@shared/header/header';
+import { DeviceTypeService } from '@core/services/device-type-service';
+import { MENU_CONSTANTS } from '@constants/menu.constants';
 
 @Component({
   selector: 'shared-menu',
@@ -19,16 +21,33 @@ import { Header } from '@shared/header/header';
 export class Menu implements OnInit {
   private readonly menuService = inject(MenuService);
   private readonly navigationService = inject(NavigationService);
+  private readonly deviceTypeService = inject(DeviceTypeService);
   public readonly drawer = viewChild.required<MatDrawer>('drawer');
+  public readonly containerDrawer = viewChild.required<MatDrawer>('containerDrawer');
   private readonly _isOpenSlideMenu = signal<boolean>(true);
-  public readonly isOpenSlideMenu = computed(() => this._isOpenSlideMenu());
   private readonly _openSubMenuIndex = signal<number | null>(null);
+  private readonly _hasBackdrop = signal<boolean | null>(false);
+  public readonly isOpenSlideMenu = computed(() => this._isOpenSlideMenu());
   public readonly openSubMenuIndex = computed(() => this._openSubMenuIndex());
   public readonly currentNameRoute = computed(() => this.menuService.getCurrentNameRoute());
+  public readonly typeDevice = computed(() => this.deviceTypeService.device());
+  public readonly hasBackdrop = computed(() => this._hasBackdrop());
   public readonly ICONS = ICONS;
   public readonly menuItems = computed<MenuItem[]>(() => this.menuService.menuItems());
   public readonly iconApp = environment.ICON_APP_URL;
   public readonly titleApp = environment.appName;
+
+  constructor() {
+    effect(() => {
+      if (this.typeDevice() === MENU_CONSTANTS.MOBILE) {
+        this._isOpenSlideMenu.set(false);
+        this._hasBackdrop.set(true);
+        return;
+      }
+      this._hasBackdrop.set(false);
+      this._isOpenSlideMenu.set(true);
+    });
+  }
 
   ngOnInit() {
     this._openSubMenuIndex.set(this.menuService.getCurrentIndexRoute());
