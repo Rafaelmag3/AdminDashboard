a5bff552d62d4df22dcd2423410992731414a690
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-04-05 13:54:45 -0600

    feat: fix loading interceptor import and add loading interceptor implementation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { loadingInterceptorInterceptor } from '@core/interceptors/loading-interceptor-interceptor';
+import { loadingInterceptor } from '@core/interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withInterceptors([loadingInterceptorInterceptor])),
+    provideHttpClient(withInterceptors([loadingInterceptor])),
     provideRouter(routes)
   ]
 };
--- src/app/core/interceptors/loading-interceptor.ts
+++ src/app/core/interceptors/loading-interceptor.ts
@@ -3,7 +3,7 @@ import { inject } from '@angular/core';
 import { LoadingService } from '../services/loading-service';
 import { finalize } from 'rxjs';
 
-export const loadingInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoadingService);
   loadingService.show();
   return next(req).pipe(finalize(() => loadingService.hide()));
