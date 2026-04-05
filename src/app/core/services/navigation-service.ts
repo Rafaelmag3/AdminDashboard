111bcddab5a6942292c8aa84ed0b1e5414eda6cf
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-04-05 17:15:26 -0600

    feat: refactor NavigationService to use Angular's inject function for router dependency

--- src/app/core/services/navigation-service.ts
+++ src/app/core/services/navigation-service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NavigationService {
-
-  constructor(private router: Router) { }
+  private readonly router = inject(Router);
 
   public navigateTo(path: string): void {
     this.router.navigate([path]);
