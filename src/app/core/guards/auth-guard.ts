3d373339c02fe1146b7ae4d13d4cf3ee95f2a591
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-04-05 17:01:29 -0600

    feat: remove unused navigation service from auth guard and clean up logout logic

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,16 +1,13 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { UserStoreService } from '@core/services/user-store-service';
-import { NavigationService } from '@core/services/navigation-service';
 
 export const authGuard: CanActivateFn = async () => {
   const userStoreService = inject(UserStoreService);
-  const navigationService = inject(NavigationService);
   await userStoreService.initUser();
   const token = userStoreService.token();
   if (!token) {
     userStoreService.logout();
-    navigationService.navigateTo('login');
     return false;
   }
 
