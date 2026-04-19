084c2e6017464c3fc22b64e54359f81f722f7b40
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-04-18 19:44:26 -0600

    fix: Creating path constants

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,34 +2,41 @@ import { Routes } from '@angular/router';
 import { authGuard } from '@core/guards/auth-guard';
 import { loginGuard } from '@core/guards/login-guard';
 import { ICONS } from './constants/icons.contants';
+import { PATH_CONSTANTS } from '@constants/path.constants';
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
+        path: PATH_CONSTANTS.ROOT,
+        redirectTo: PATH_CONSTANTS.DASHBOARD,
+        pathMatch: PATH_CONSTANTS.FULL
     },
     {
-        path: 'login',
+        path: PATH_CONSTANTS.LOGIN,
         canActivate: [loginGuard],
         loadComponent: () => import('@features/auth/pages/login/login').then(m => m.Login)
     },
     {
-        path: '',
+        path: PATH_CONSTANTS.ROOT,
         data: { name: '', showInMenu: true },
         canActivate: [authGuard],
         loadComponent: () => import('@shared/layout/layout').then(m => m.Layout),
         children: [
             {
-                path: 'dashboard',
+                path: PATH_CONSTANTS.DASHBOARD,
                 canActivate: [authGuard],
                 data: { name: 'Dashboard', showInMenu: true, icon: ICONS.DASHBOARD },
                 loadComponent: () => import('@features/dashboard/page/dashboard/dashboard').then(m => m.Dashboard)
+            },
+            {
+                path: PATH_CONSTANTS.CUSTOMERS,
+                canActivate: [authGuard],
+                data: { name: 'Customers', showInMenu: true, icon: ICONS.CUSTOMERS },
+                loadComponent: () => import('@features/customers/page/customers/customers').then(m => m.Customers)
             }
         ]
     },
     {
-        path: '**',
-        redirectTo: 'dashboard'
+        path: PATH_CONSTANTS.ALL,
+        redirectTo: PATH_CONSTANTS.DASHBOARD
     }
 ];
---
+++ src/app/constants/path.constants.ts
@@ -0,0 +1,8 @@
+export const PATH_CONSTANTS = {
+    ROOT: '',
+    ALL: '**',
+    FULL: 'full',
+    LOGIN: 'login',
+    DASHBOARD: 'dashboard',
+    CUSTOMERS: 'customers'
+} as const;
