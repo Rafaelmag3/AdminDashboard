f5c66a4b205f006de3b390d02d3369d7aaab3a9d
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-04-18 19:50:08 -0600

    fix: Position card products

--- src/app/features/dashboard/page/dashboard/dashboard.ts
+++ src/app/features/dashboard/page/dashboard/dashboard.ts
@@ -16,16 +16,16 @@ export class Dashboard {
   public readonly metrics = computed(() => this.dashboardService.metrics());
   public readonly cardsDashboard = computed<DashboardCardItem[]>(() => [
     {
-      title: 'Total Services',
-      total: this.metrics()?.totalServices ?? 0,
-      icon: ICONS.SERVICES,
-      stylesIconCard: 'bg-blue-100 text-blue-500 rounded-full p-2 flex items-center justify-center',
-    },
-    {
       title: 'Total Customers',
       total: this.metrics()?.totalCustomers ?? 0,
       icon: ICONS.CUSTOMERS,
       stylesIconCard: 'bg-green-100 text-green-500 rounded-full p-2 flex items-center justify-center',
+    },
+    {
+      title: 'Total Producs',
+      total: this.metrics()?.totalServices ?? 0,
+      icon: ICONS.SERVICES,
+      stylesIconCard: 'bg-blue-100 text-blue-500 rounded-full p-2 flex items-center justify-center',
     },
   ]);
 }
