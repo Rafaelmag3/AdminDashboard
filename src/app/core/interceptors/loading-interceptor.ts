9883e98f5d19cf64591572f23d23d27b16c05d45
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2026-05-03 13:10:57 -0600

    fix: Using alias import

--- src/app/core/interceptors/loading-interceptor.ts
+++ src/app/core/interceptors/loading-interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { LoadingService } from '../services/loading-service';
+import { LoadingService } from '@core/services/loading-service';
 import { catchError, finalize, throwError } from 'rxjs';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
